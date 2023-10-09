d6e74bdaa74386339289a2724c0f41356888c464
Masdikaa <masdikailhanmansiz@gmail.com>
2023-10-09 14:04:56 +0700

    First Commit

---
+++ resources/views/layouts/mitra-main.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+    <title>Main_Mitrakom</title>
+
+    <meta name="description" content="OneUI - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+    <meta name="author" content="pixelcave">
+    <meta name="robots" content="noindex, nofollow">
+
+    <!-- Icons -->
+    <link rel="shortcut icon" href="{{ asset('media/favicons/favicon.png') }}">
+    <link rel="icon" sizes="192x192" type="image/png" href="{{ asset('media/favicons/favicon-192x192.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('media/favicons/apple-touch-icon-180x180.png') }}">
+
+    <!-- Modules -->
+    @yield('css')
+    @vite(['resources/sass/main.scss', 'resources/js/oneui/app.js'])
+
+    @yield('js')
+</head>
+
+<body>
+
+    <div id="page-container" class="sidebar-o enable-page-overlay sidebar-dark side-scroll page-header-fixed main-content-narrow">
+        <!-- Sidebar -->
+        @include('partials.sidebar')
+        <!-- END Sidebar -->
+
+        <!-- Header -->
+        @include('partials.header')
+        <!-- END Header -->
+
+        <!-- Main Container -->
+        <main id="main-container">
+            @yield('content')
+        </main>
+        <!-- END Main Container -->
+
+        <!-- Footer -->
+        @include('partials.footer')
+        <!-- END Footer -->
+    </div>
+
+</body>
+
+</html>
