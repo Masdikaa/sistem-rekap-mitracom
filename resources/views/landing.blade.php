8f42377d28ca7318b0da2a153f434b6244340795
Masdikaa <masdikailhanmansiz@gmail.com>
2023-10-10 16:43:19 +0700

    View login and fixing main interface

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,22 +1,66 @@
 @extends('layouts.simple')
 
 @section('content')
-  <!-- Hero -->
-  <div class="hero bg-body-extra-light overflow-hidden">
-    <div class="hero-inner">
-      <div class="content content-full text-center">
-        <h1 class="fw-bold mb-2">
-          OneUI <span class="fw-normal">+ Laravel <span class="text-city">9</span></span>
-        </h1>
-        <p class="fs-lg fw-medium text-muted mb-4">
-          Welcome to the starter kit! Build something amazing!
-        </p>
-        <a class="btn btn-alt-primary px-3 py-2" href="/dashboard">
-          Enter Dashboard
-          <i class="fa fa-fw fa-arrow-right opacity-50 ms-1"></i>
-        </a>
-      </div>
+    <!-- Hero -->
+    <div class="hero bg-body-extra-light overflow-hidden">
+        <div class="hero-inner">
+
+            {{-- Container Form --}}
+            <div class="content content-full text-center">
+
+                <img class="my-4" src="{{ asset('build/assets/mitracom-icon.png') }}"
+                    style=" width: 30%;
+                      height: 30%;
+                      max-width: 100%;
+                      max-height: 100%;
+                     "
+                    alt="Mitracom">
+
+                {{-- <h4 class="fw-bold mb-2">
+                    Mitra<span class="fw-normal"><span class="text-city">com</span></span>
+                </h4>
+
+                <p class="fs-lg fw-medium text-muted mb-4">
+                    Solusi Problem Komputer Anda
+                </p> --}}
+
+                <div class="row justify-content-center mb-4">
+
+                    <div class="col-md-6 col-xl-4">
+                        <div class="block block-rounded h-100 mb-0">
+                            <div class="block-header block-header-default">
+                                <h3 class="block-title text-start">Sign In to Your Account</h3>
+                            </div>
+                            <div class="block-content fs-sm p-3">
+
+                                <form>
+
+                                    <div class="form-group text-start">
+                                        <label class="text-muted" for="exampleInputEmail1">
+                                            Username
+                                        </label>
+                                        <input type="text" class="form-control" id="inputUsername" aria-describedby="emailHelp">
+                                    </div>
+
+                                    <div class="form-group text-start mt-4 md-4">
+                                        <label class="text-muted" for="exampleInputPassword1">Password</label>
+                                        <input type="password" class="form-control" id="exampleInputPassword1">
+                                    </div>
+
+                                    <a class="btn btn-alt-primary mt-4 px-3 py-2" href="/dashboard">
+                                        Login
+                                    </a>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            {{-- End Container Form --}}
+        </div>
     </div>
-  </div>
-  <!-- END Hero -->
+    <!-- END Hero -->
 @endsection
--- resources/views/layouts/mitra-main.blade.php
+++ resources/views/layouts/mitra-main.blade.php
@@ -15,10 +15,10 @@
 
     {{-- data table --}}
     <link rel="stylesheet" href="{{ asset('js/plugins/datatables-bs5/css/dataTables.bootstrap5.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('js/plugins/datatables-buttons-bs5/css/buttons.bootstrap5.min.css') }}"> 
+    <link rel="stylesheet" href="{{ asset('js/plugins/datatables-buttons-bs5/css/buttons.bootstrap5.min.css') }}">
     <!-- Page JS Plugins CSS -->
     <link rel="stylesheet" href="{{ asset('js/plugins/datatables-bs5/css/dataTables.bootstrap5.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('js/plugins/datatables-buttons-bs5/css/buttons.bootstrap5.min.css') }}">  
+    <link rel="stylesheet" href="{{ asset('js/plugins/datatables-buttons-bs5/css/buttons.bootstrap5.min.css') }}">
 
     <!--BS ICON-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
@@ -29,12 +29,12 @@
 
     @yield('js')
 
-    
+
 </head>
 
 <body>
 
-    <div id="page-container" class="sidebar-o enable-page-overlay sidebar-dark side-scroll page-header-fixed main-content-narrow">
+    <div id="page-container" class="sidebar-o enable-page-overlay sidebar-dark side-scroll page-header-fixed main-content-boxed">
         <!-- Sidebar -->
         @include('partials.sidebar')
         <!-- END Sidebar -->
@@ -54,22 +54,22 @@
         <!-- END Footer -->
     </div>
 
-     <!-- jQuery (required for DataTables plugin) -->
-  <script src="{{ asset('js/lib/jquery.min.js') }}"></script>
+    <!-- jQuery (required for DataTables plugin) -->
+    <script src="{{ asset('js/lib/jquery.min.js') }}"></script>
 
-  <!-- Page JS Plugins -->
-  <script src="{{ asset('js/plugins/datatables/jquery.dataTables.min.js') }}"></script>
-  <script src="{{ asset('js/plugins/datatables-bs5/js/dataTables.bootstrap5.min.js') }}"></script>
-  <script src="{{ asset('js/plugins/datatables-buttons/dataTables.buttons.min.js') }}"></script>
-  <script src="{{ asset('js/plugins/datatables-buttons-bs5/js/buttons.bootstrap5.min.js') }}"></script>
-  <script src="{{ asset('js/plugins/datatables-buttons-jszip/jszip.min.js') }}"></script>
-  <script src="{{ asset('js/plugins/datatables-buttons-pdfmake/pdfmake.min.js') }}"></script>
-  <script src="{{ asset('js/plugins/datatables-buttons-pdfmake/vfs_fonts.js') }}"></script>
-  <script src="{{ asset('js/plugins/datatables-buttons/buttons.print.min.js') }}"></script>
-  <script src="{{ asset('js/plugins/datatables-buttons/buttons.html5.min.js') }}"></script>
+    <!-- Page JS Plugins -->
+    <script src="{{ asset('js/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/datatables-bs5/js/dataTables.bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/datatables-buttons/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/datatables-buttons-bs5/js/buttons.bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/datatables-buttons-jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/datatables-buttons-pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/datatables-buttons-pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('js/plugins/datatables-buttons/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/datatables-buttons/buttons.html5.min.js') }}"></script>
 
-  <!-- Page JS Code -->
-  @vite(['resources/js/pages/datatables.js'])
+    <!-- Page JS Code -->
+    @vite(['resources/js/pages/datatables.js'])
 
 </body>
 
